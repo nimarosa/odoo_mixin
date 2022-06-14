6c18526039820119ee43de877268d75a533f42a6
nicolasrsande <nicolasrsande@gmail.com>
2022-06-14 16:48:42 -0300

    merge changes, not usable for now

---
+++ l10n_ar_advanced_reports/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2022 Nimarosa (Nicolas Rodriguez) (<nicolasrsande@gmail.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Odoo Advanced Reports (Argentina)',
+    'version': '14.0.1.0.0',
+    'description': 'QWeb reports styling for odoo modules.',
+    'summary': 'This module adds new qweb reports (only for Argentinian Localization) for several odoo modules.',
+    'author': 'Nimarosa (http://nimarosa.dev/)',
+    'website': 'nimarosa.dev',
+    'license': 'LGPL-3',
+    'category': 'Reporting',
+    'depends': [
+        'base',
+        'account'
+    ],
+    'data': [
+        ''
+    ],
+    'installable': True,
+    'auto_install': False,
+}
